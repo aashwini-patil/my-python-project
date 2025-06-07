fa9b342cf765eb2ed20e0536fa8cce9e8d07f062
aashwini-patil <patilashwini191010@gmail.com>
2025-06-07 19:40:22 +0530

    Delete personal_greeting.py

--- personal_greeting.py
+++
@@ -1,4 +0,0 @@
-firstName = input("Enter your first name: ")
-lastName = input("Enter your last name: ")
-fullName = firstName + " " + lastName + "!"
-print("Hello, "+ fullName +" Welcome to the Python program.")
