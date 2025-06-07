2e5f3ec4f21b11b06d361295f741686bfadf3fc5
Ashwini Patil <patilashwini191010@gmail.com>
2025-06-07 20:09:48 +0530

    add 1st assignment

---
+++ Assignment-1/calculator.py
@@ -0,0 +1,17 @@
+'''
+Task 1: Perform Basic Mathematical Operations
+
+Problem Statement: Write a Python program that does the following:
+
+Takes two numbers as input from the user.
+Performs the basic mathematical operations on these two numbers: o Addition o Subtraction o Multiplication o Division
+Displays the results of each operation on the screen.
+
+'''
+n1=int(input("Enter the first number: "))
+n2=int(input("Enter the second number: "))
+
+print("\n\nAddition: ",n1+n2)
+print("Subtraction: ",n1-n2)
+print("Multiplication: ",n1*n2)
+print("Division: ",n1/n2)
---
+++ Assignment-1/personal_greeting.py
@@ -0,0 +1,14 @@
+'''
+Task 2: Create a Personalized Greeting
+
+Problem Statement: Write a Python program that:
+
+Takes a user's first name and last name as input.
+Concatenates the first name and last name into a full name.
+Prints a personalized greeting message using the full name.
+'''
+
+firstName = input("Enter your first name: ")
+lastName = input("Enter your last name: ")
+fullName = firstName+" "+lastName+"!"
+print("Hello, "+fullName+" Welcome to the Python program.")
