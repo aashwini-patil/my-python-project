7700b6247d18f5b8cbc6292dd7f2192d2b19a48d
Ashwini Patil <patilashwini191010@gmail.com>
2025-07-08 20:40:14 +0530

    Submit Assignment-8

---
+++ Assignment-8/user_auth_project/auth_app/views.py
@@ -0,0 +1,38 @@
+
+# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import RegisterForm
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Registration successful!')
+            return redirect('login')
+    else:
+        form = RegisterForm()
+    return render(request, 'auth_app/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid credentials')
+    return render(request, 'auth_app/login.html')
+
+@login_required
+def home_view(request):
+    return render(request, 'auth_app/home.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
